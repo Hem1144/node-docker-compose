c619d5d574ca74abfec33bd703db3ae092ae3b3b
Hem1144 <hemlaldulal7@gmail.com>
2024-06-22 23:35:11 +0545

    feat: Maintained enviromnent variable and its operation

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require("express");
+require("dotenv").config();
 
 const app = express();
 const PORT = 5000;
 
 app.get("/", (req, res) => {
-  res.send("Welcome to my docker-compose app!");
+  res.send(
+    "Welcome to my docker-compose app!" +
+      process.env.APP +
+      "db_name" +
+      process.env.DB_NAME
+  );
 });
 
 app.listen(PORT, () => {
